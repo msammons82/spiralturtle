6454ee33e0b7f5944b01b27606b11218320e2d86
msammons82 <87323843+msammons82@users.noreply.github.com>
2022-01-30 12:47:50 -0500

    Add files via upload

---
+++ spiralTurtle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python 3
+# A program to make spiral graphics with turtle python
+
+import random
+import turtle
+
+colors = ['red', 'cyan', 'pink', 'yellow', 'green', 'orange']
+t = turtle.Turtle()
+t.speed(10)
+turtle.bgcolor("black")
+length = 100
+angle = 50
+size = 5
+for i in range(length):
+    color = random.choice(colors)
+    t.pencolor(color)
+    t.fillcolor(color)
+    t.penup()
+    t.forward(i+50)
+    t.pendown()
+    t.left(angle)
+    t.begin_fill()
+    t.circle(size)
+    t.end_fill()
+turtle.exitonclick()
+turtle.bgcolor("black")
